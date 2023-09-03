4a01a99f0bf9bac4384ad6e7a233b3a6f548886f
Felipe Sanches <felipesanches@edu.unifil.br>
2023-09-03 12:41:28 -0300

    Atualizações

---
+++ src/Trabalho_1509/Cachorro.java
@@ -0,0 +1,11 @@
+package Trabalho_1509;
+
+public class Cachorro extends Animal {
+    private String corDoPelo;
+
+    public Cachorro(String nome, int idade, String corDoPelo) {
+        super();
+        this.corDoPelo = corDoPelo;
+    }
+
+}
---
+++ src/Trabalho_1509/Gato.java
@@ -0,0 +1,6 @@
+package Trabalho_1509;
+
+public class Gato {
+    private String corDoPelo;
+
+}
