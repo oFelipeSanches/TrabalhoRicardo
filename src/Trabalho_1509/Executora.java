6e40817b7a006641bae8c2eb781ed3621b5038b4
Felipe Sanches <felipesanches@edu.unifil.br>
2023-09-03 13:30:33 -0300

    Atualizações

--- src/Trabalho_1509/Executora.java
+++ src/Trabalho_1509/Executora.java
@@ -2,13 +2,14 @@ package Trabalho_1509;
 import java.util.Scanner;
 public class Executora {
     public static void main(String[] args) {
+
         int n1;
 
         System.out.println("---------------------------------------");
         System.out.println("Escolha uma espécie abaixo:");
         System.out.println("Mamífero - 1");
         System.out.println("Ovíparo - 2");
-        System.out.println("Digite o número conforme o que você quer: ");
+        System.out.print("Digite o número conforme o que você quer: ");
         Scanner scan = new Scanner(System.in);
         n1 = scan.nextInt();
 
