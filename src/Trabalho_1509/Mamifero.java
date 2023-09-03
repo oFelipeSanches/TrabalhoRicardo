880f4b0118c9b2b935b6b9fcc91299b71ada9dea
Felipe Sanches <felipesanches@edu.unifil.br>
2023-09-03 12:52:36 -0300

    Atualizações

---
+++ src/Trabalho_1509/Mamifero.java
@@ -0,0 +1,14 @@
+package Trabalho_1509;
+
+public class Mamifero extends Animal {
+    private String corDoPelo;
+
+    public Mamifero(String nome, int idade, String corDoPelo) {
+        super(nome, idade);
+        this.corDoPelo = corDoPelo;
+    }
+
+    public String getCorDoPelo() {
+        return corDoPelo;
+    }
+}
