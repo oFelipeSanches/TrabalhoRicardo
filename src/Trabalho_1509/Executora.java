a5384ed118f4acc6adee0df048d96a95ef1f8949
Felipe Sanches <felipesanches@edu.unifil.br>
2023-09-04 20:44:06 -0300

    Atualizações

--- src/Trabalho_1509/Executora.java
+++ src/Trabalho_1509/Executora.java
@@ -11,7 +11,74 @@ public class Executora {
         System.out.println("Ovíparo - 2");
         System.out.print("Digite o número conforme o que você quer: ");
         Scanner scan = new Scanner(System.in);
+
         escolha = scan.nextInt();
+        boolean sair = false;
+
+        if (escolha == 1) {
+            while (!sair) {
+                System.out.println("Escolha uma opção:");
+                System.out.println("Criar Cachorro - 1");
+                System.out.println("Criar   Gato   - 2");
+                System.out.println("Criar   Porco  - 3");
+                System.out.println("Sair do menu   - 0");
+
+                int op = scan.nextInt();
+                scan.nextLine();
+
+                switch (op) {
+                    case 1:
+                        System.out.print("Digite o nome do cachorro: ");
+                        String nomeCachorro = scan.nextLine();
+                        System.out.print("Digite a idade do cachorro: ");
+                        int idadeCachorro = scan.nextInt();
+                        scan.nextLine();
+                        System.out.print("Digite a cor do pelo do cachorro: ");
+                        String corPeloCachorro = scan.nextLine();
+                        Cachorro cachorro = new Cachorro(nomeCachorro, idadeCachorro, corPeloCachorro);
+
+                        cachorro.somAnimal();
+                        break;
+
+                    case 2:
+                        System.out.print("Digite o nome do gato: ");
+                        String nomeGato = scan.nextLine();
+                        System.out.print("Digite a idade do gato: ");
+                        int idadeGato = scan.nextInt();
+                        scan.nextLine();
+                        System.out.print("Digite a cor do pelo do gato: ");
+                        String corPeloGato = scan.nextLine();
+                        Gato gato = new Gato(nomeGato, idadeGato, corPeloGato);
+
+                        gato.somAnimal();
+                        break;
+
+                    case 3:
+                        System.out.print("Digite o nome do Porco: ");
+                        String nomePorco = scan.nextLine();
+                        System.out.print("Digite a idade do Porco: ");
+                        int idadePorco = scan.nextInt();
+                        System.out.print("Digite a cor do Porco: ");
+                        String corPorco = scan.nextLine();
+                        Porco porco = new Porco(nomePorco, idadePorco, corPorco);
+
+                        porco.somAnimal();
+                        break;
+
+                    case 0:
+                        sair = true;
+                        break;
+
+                    default:
+                        System.out.println("Opcao invalida");
+                        break;
+                }
+
+            }
+        }
 
     }
+
+
+
 }
--- src/Trabalho_1509/If.java
+++ src/Trabalho_1509/If.java
@@ -1,18 +1,5 @@
 package Trabalho_1509;
 
-public class If extends Escolha{
-    boolean sair = false;
+public class If {
 
-    public If(int escolha, boolean sair) {
-        super();
-        this.sair = sair;
-    }
-
-    public boolean isSair() {
-        return sair;
-    }
-
-    public void setSair(boolean sair) {
-        this.sair = sair;
-    }
 }
