eb4bda21f9803f3af34ae1476df21dae1ac367e2
Felipe Sanches <felipesanches@edu.unifil.br>
2023-09-11 20:53:42 -0300

    Atualizações

--- src/Trabalho_1509/Executora.java
+++ src/Trabalho_1509/Executora.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class Executora {
     public static void main(String[] args) {
 
-        int escolha;
+            int escolha;
 
-        System.out.println("---------------------------------------");
-        System.out.println("Escolha uma espécie abaixo:");
-        System.out.println("Mamífero - 1");
-        System.out.println("Ovíparo - 2");
-        System.out.print("Digite o número conforme o que você quer: ");
-        Scanner scan = new Scanner(System.in);
+            System.out.println("---------------------------------------");
+            System.out.println("Escolha uma espécie abaixo:");
+            System.out.println("Mamífero - 1");
+            System.out.println("Ovíparo - 2");
+            System.out.print("Digite o número conforme o que você quer: ");
+            Scanner scan = new Scanner(System.in);
 
-        escolha = scan.nextInt();
+            escolha = scan.nextInt();
 
-        Funcionalidades choose = new Funcionalidades();
-        choose.definirTipo(escolha);
+            Funcionalidades choose = new Funcionalidades();
+            choose.definirTipo(escolha);
 
     }
 }
